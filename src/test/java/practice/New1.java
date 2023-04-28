a35df975b12feb62ccf88eeeb4558c1f93cebe26
Chandan Kumar <Chandan Kumar@DESKTOP-6KTKCSQ>
2023-04-28 09:15:31 +0530

    commit todones

---
+++ src/test/java/practice/New1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class New1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("our new github");
+		
+		
+		
+	}
+
+}
